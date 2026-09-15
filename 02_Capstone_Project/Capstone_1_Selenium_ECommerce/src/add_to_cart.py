eb9776be0e4e6c090efff09b18a20ed64686875e
dev-ujjawal <ujjawalk141@gmail.com>
2026-09-15 17:37:24 +0530

    updated product search.py

--- 02_Capstone_Project/Capstone_1_Selenium_ECommerce/src/add_to_cart.py
+++ 02_Capstone_Project/Capstone_1_Selenium_ECommerce/src/add_to_cart.py
@@ -1,37 +1,111 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
 
-# Start the browser
+# start the chrome browser
 driver = webdriver.Chrome()
 
-# Open tutorialNinja Homepage
-driver.get("https://tutorialsninja.com/demo/")
+# open the automation exercise homepage
+driver.get("https://automationexercise.com/")
 
-# Locate the search box
-search_box = driver.find_element(By.NAME, "search")
+# read product and quantity from JSON test data
+with open("../test_data/test_data.json", "r") as file:
+    data = json.load(file)
 
-#search for any product
-search_box.send_keys("iphone")
+product = data["product"]
+quantity = str(data["quantity"])
 
-# Now click the search Button
-driver.find_element(By.CSS_SELECTOR, "button.btn.btn-default.btn-lg").click()
-
-# Wait for search results
-time.sleep(2)
-
-# Locate the iPhone Add to Cart button
-add_to_cart = driver.find_element(
+# click product
+driver.find_element(
     By.CSS_SELECTOR,
-    "button[onclick^=\"cart.add('40'\"]"
+    "a[href='/products']"
+).click()
+
+# wait for the product page to load
+WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.XPATH, "//h2[normalize-space()='All Products']")
+    )
 )
 
+# Search for the product
+search_box = driver.find_element(By.ID, "search_product")
+
+search_box.send_keys(product)
+
+driver.find_element(
+    By.ID,
+    "submit_search"    
+).click()
+
+# wait for the search result
+WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.ID, "quantity")
+    )
+)
+
+# open men tshirt product details
+driver.find_element(
+    By.CSS_SELECTOR,
+    "a[href='/product_details/2']"
+).click()
+
+# wait for the quantity field on product page
+quantity_input = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.ID, "quantity")
+    )
+)
+
+# verify correct product
+product_id = driver.find_element(
+    By.ID,
+    "product_id"
+).get_attribute("value")
+
+
+if product_id == "2":
+    print("Men Tshirt product opened successfully.")
+else:
+    print("Incorrect product opened.")
+
+# Change quantity to 4
+quantity_input.clear()
+quantity_input.send_keys(quantity)
+
+print("Quantity set to:", quantity)
+
 # Click Add to Cart
-add_to_cart.click()
+driver.find_element(
+    By.CSS_SELECTOR,
+    "button.cart"
+).click()
 
-# Wait so the cart update can be observed
-time.sleep(3)
+# Handle Add to Cart popup
+continue_shopping = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "button.close-modal")
+    )
+)
 
-print("Product added to cart.")
+print("Add to Cart popup displayed.")
+
+continue_shopping.click()
+
+# Open Cart
+driver.find_element(
+    By.CSS_SELECTOR,
+    "a[href='/view_cart']"
+).click()
+
+# Wait for cart page
+WebDriverWait(driver, 10).until(
+    EC.url_contains("/view_cart")
+)
+
+print("Cart opened successfully.")
 
 driver.quit()
--- 02_Capstone_Project/Capstone_1_Selenium_ECommerce/src/product_search.py
+++ 02_Capstone_Project/Capstone_1_Selenium_ECommerce/src/product_search.py
@@ -1,27 +1,58 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import json
 
 # Start the chrome browser 
 driver = webdriver.Chrome()
 
-# Now open the tutorialNinja Homepage
-driver.get("https://tutorialsninja.com/demo/")
+# Now open the automation exercise Homepage
+driver.get("https://automationexercise.com/")
 
-# Locate the search box using name attribute
-search_box = driver.find_element(By.NAME, "search")
+# reading product name from json test_data
+with open ("../test_data/test_data.json","r") as file:
+    data = json.load(file)
 
-# search for a product
-search_box.send_keys("iphone")
+product = data["product"]
 
-# click the search button to search
-driver.find_element(By.CSS_SELECTOR, "button.btn.btn-default.btn-lg").click()
+# click on products
+driver.find_element(By.CSS_SELECTOR, "a[href='/products']").click()
 
-#Adding a sleeping time to wait for some time to obseve the result
-time.sleep(10)
+# wait untill all product page is laoded
+all_products = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.XPATH, "//h2[normalize-space()='All Products']")
+    )
+)
 
-# Display the result in cmd
-print("Page title: ", driver.title)
-print("Current URL: ", driver.current_url)
+print("All product page displayed")
+
+# Enter product name in search box
+search_box = driver.find_element(By.ID, "search_product")
+
+search_box.send_keys(product)
+
+# click on search button
+driver.find_element(By.ID, "submit_search").click()
+
+# Wait until Searched Products heading appears
+searched_products = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.XPATH, "//h2[normalize-space()='Searched Products']")
+    )
+)
+
+print("Searched product heading displayed")
+
+# verify searched product is visible or not
+product_element = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, "a[href='/product_details/2']")
+    )
+)
+
+print("Product found: ", product)
+print("Product search successful.")
 
 driver.quit()
